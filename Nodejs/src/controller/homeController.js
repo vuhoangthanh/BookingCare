11eb31987e33a5e091f9ab7667384fd26e0667b6
thanh <vuhoangthanh0710@gmail.com>
2025-02-10 16:51:44 +0700

    start react

--- Nodejs/src/controller/homeController.js
+++ Nodejs/src/controller/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index';
+import CRUDService from '../service/CRUDService';
 
 let getHomePage = async (req, res) => {
     try {
@@ -17,11 +18,68 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
-// object: {
-//     key: '',
-//     value: ''
-// }
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+}
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    console.log('---------------')
+    console.log(data)
+    console.log('---------------')
+    return res.render('display-CRUD.ejs', {
+        dataTable: data
+    })
+}
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        //check user data not found
+
+        return res.render('editCRUD.ejs', {
+            user: userData
+        })
+    } else {
+        return res.send("hellow")
+    }
+
+}
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updataUserData(data);
+    //check user data not found
+
+    return res.render('display-CRUD.ejs', {
+        dataTable: allUsers
+    })
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('delete the user succeed')
+    } else {
+        return res.send('user not found')
+    }
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
